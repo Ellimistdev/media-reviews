970e1d1343f98253e77e258d561a6d8903b948f7
endotnick <endotnick@gmail.com>
2019-01-27 17:22:25 -0800

    adds missing associations between series, seasons, and episodes

--- app/models/season.rb
+++ app/models/season.rb
@@ -2,6 +2,7 @@ class Season < ApplicationRecord
   include Conceivable
   belongs_to :medium
   belongs_to :series
+  has_many :episodes
   before_validation :build_parents
   validates :number, presence: true
 end
--- app/models/series.rb
+++ app/models/series.rb
@@ -1,6 +1,8 @@
 class Series < ApplicationRecord
   include Conceivable
   belongs_to :medium  
+  has_many :seasons
+  has_many :episodes, through: :seasons
   before_validation :build_parents
   validates :title, presence: true
 end
