d1d33d7998fe2cd5eaea96723ecd23df9a6f9ee1
endotnick <endotnick@gmail.com>
2019-01-27 22:16:19 -0800

    adds scope method to medium

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -3,7 +3,9 @@ class Medium < ApplicationRecord
   has_many :reviewers, through: :reviews, source: :reviewer
   has_many :views
   has_many :viewers, through: :views, source: :viewer
-
+  scope :reviewed, -> { Medium.joins(:reviews) }
+  scope :not_reviewed, -> { Medium.joins("LEFT JOIN reviews ON media.id = reviews.medium_id WHERE reviews.medium_id IS NULL") }
+  
   # Searches the respective media table for its id
   # e.g. if medium_type = 'Episode' query becomes: 
   # Episode.find_by(medium_id: id)
