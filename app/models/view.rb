25b3e0d96200622c09de901a4e793fd36232bd87
endotnick <endotnick@gmail.com>
2019-01-30 19:03:05 -0800

    adds views validation

--- app/models/view.rb
+++ app/models/view.rb
@@ -1,4 +1,7 @@
 class View < ApplicationRecord
   belongs_to :viewer, class_name: 'User', foreign_key: :user_id
   belongs_to :medium
+  validates :viewer, prescence: true
+  validates :medium, prescence: true
+  validates_uniqueness_of :viewer, scope: :medium
 end
