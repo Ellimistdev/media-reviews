4a44ecfe1f2139b3d70e685dbdea2920eddf338a
endotnick <endotnick@gmail.com>
2019-01-31 13:32:40 -0800

    update review form to automatically select medium and user

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -12,12 +12,10 @@ class ReviewsController < ApplicationController
 
   def create
     review = Review.new(review_params)
-    review.medium = Medium.find(params[:medium_id])
-    review.reviewer = current_user
     review.save
     # If this fails, view already exists
     View.create(viewer: review.reviewer, medium: review.medium)
-    return redirect_back fallback_location: medium_path(review.medium), notice: @review.errors unless @review.errors.blank
+    return redirect_back fallback_location: medium_path(review.medium), notice: review.errors unless review.errors.blank?
 
     redirect_to medium_path(review.medium)
   end
@@ -39,7 +37,7 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:content, :rating)
+    params.require(:review).permit(:content, :rating, :medium_id, :user_id)
   end
 
   def require_owner
