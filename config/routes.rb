c5a6146cb5fe57677167849afdf81eb99ecc1c8c
endotnick <endotnick@gmail.com>
2019-01-30 17:07:14 -0800

    adjusts routing, fixes hardcodes routing in views/forms

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,12 @@ Rails.application.routes.draw do
 
   # User Routes
   get '/signup' => 'users#new'
-  post '/signup' => 'users#create'
-  resources :users, only: %i[show update] do 
+  resources :users, only: %i[show update create] do 
     resources :reviews, only: %i[index] 
     resources :views, only: %i[index]
   end
-  resources :reviews, only: %i[destroy create edit update] 
-  resources :views, only: %i[destroy create]
+  resources :reviews, only: %i[create edit update destroy]
+  resources :views, only: %i[create destroy]
 
   # Media Routes
   resources :media, only: %i[index show] 
@@ -19,13 +18,13 @@ Rails.application.routes.draw do
       resources :episodes, only: %i[index]
     end
   end  
-  resources :seasons, only: %i[show create new]
-  resources :episodes, only: %i[show create new]
+  resources :seasons, only: %i[new create show]
+  resources :episodes, only: %i[new create show]
 
   # Session Control
   get '/login/' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  post '/logout' => 'sessions#destroy'
+  post '/logout/' => 'sessions#destroy'
+  resources :sessions, only: %i[create]
   # omniauth
   get '/auth/:provider/callback' => 'sessions#omni_create'
 end
