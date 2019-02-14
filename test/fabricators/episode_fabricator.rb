60ba674e0fb5fc6ac573213a9a90f3623952d136
endotnick <endotnick@gmail.com>
2019-02-13 18:04:38 -0800

    update fabricators to use existing series/seasons on occasion

--- test/fabricators/episode_fabricator.rb
+++ test/fabricators/episode_fabricator.rb
@@ -1,6 +1,9 @@
 Fabricator(:episode) do
   # Faker does not have Faker::Movie.title
   title { Faker::Book.unique.title }
-  season
+  season do
+    existing_season = Season.all.sample if [true, false].sample && Season.any?
+    existing_season || Fabricate(:season) 
+  end
   medium { Fabricate(:medium, medium_type: 'Episode') }
 end
--- test/fabricators/season_fabricator.rb
+++ test/fabricators/season_fabricator.rb
@@ -1,5 +1,8 @@
-Fabricator(:season) do
-  number { rand(1..5) }
-  series
+Fabricator(:season) do  
+  number { rand(1..100) }  
+  series do
+    existing_series = Series.all.sample if [true, false].sample && Series.any?
+    existing_series || Fabricate(:series) 
+  end
   medium { Fabricate(:medium, medium_type: 'Season') }
 end
