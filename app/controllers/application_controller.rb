6eadc0a59c0268d7d863b6b4aa64ade2efc0eee0
endotnick <endotnick@gmail.com>
2019-02-14 23:13:37 -0800

    fix line endings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,7 +29,7 @@ class ApplicationController < ActionController::Base
   # Overridden by exposed controllers
   def data
     redirect_to root_path
-  end  
+  end
 
   def collection
     redirect_to root_path
