f589e541efe049bc12351419d44622892b243808
endotnick <endotnick@gmail.com>
2019-02-14 18:37:36 -0800

    fix medium identity error in js object creation

--- app/assets/javascripts/medium.js
+++ app/assets/javascripts/medium.js
@@ -1,6 +1,6 @@
 class Medium {
   constructor(medium) {
-    this.id = medium.id;
+    this.id = medium.medium_id || medium.id;
     this.title = medium.title;
     this.children = medium.children;
     this.reviews = medium.reviews;
